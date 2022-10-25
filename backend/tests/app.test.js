317b918adc9cc409e4020d55b569def6111fe2d0
Bastian Somon <bastian.somon@gmail.com>
2022-10-25 17:32:29 +0200

    Update CI

---
+++ backend/tests/app.test.js
@@ -0,0 +1,30 @@
+const { app, server, db } = require("../server")
+const supertest = require("supertest")
+const request = supertest(app)
+const { ConnectionRefusedError } = require("sequelize")
+
+describe('/hello endpoint', () => {
+    it('should return a response', async () => {
+        const response = await request.get("/hello")
+        expect(response.status).toBe(200)
+        expect(response.text).toBe("Hello");
+    });
+});
+
+describe("test db", () => {
+    it("should return a response", async () => {
+        expect(async () => {
+            await db.sequelize.authenticate()
+        }).not.toThrow(ConnectionRefusedError);
+    });
+})
+
+
+describe("Shutting down", () => {
+    it("should close the server", () => {
+        expect(server).toBeDefined();
+        server?.close(err => {
+            expect(err).toBe(undefined);
+        })
+    })
+})
