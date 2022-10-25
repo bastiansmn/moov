dd03883ce06bc40b3602be810994a217c4ac1870
Bastian Somon <bastian.somon@gmail.com>
2022-10-25 18:11:03 +0200

    Update CI

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -3,7 +3,7 @@ module.exports = {
   USER: process.env.DB_USER || "postgres",
   PASSWORD: process.env.DB_PASSWORD || "root",
   DB: process.env.DB_NAME || "moov",
-  PORT: process.env.DB_PORT || 5433,
+  PORT: process.env.DB_PORT || 5432,
   dialect: "postgres",
   pool: {
     acquire: 4_900,
