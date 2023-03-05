0ac5bbb2db3b28cd40dffb155e235594206e24ab
Bastian SOMON <bastian.somon@gmail.com>
2023-03-05 19:10:24 +0000

    Fix CD

--- infra/create_secret.sh
+++ infra/create_secret.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-if [ "$#" !=  1  ];then
-    echo "Invalid parameters : <environment:discoodle-transverse|sandbox-discoodle|discoodle>"
-    exit 0
-else
-    NAMESPACE=$1
-fi
+SECRET_NAME="moov-secret"
 
-SECRET_NAME="discoodle-secret"
-
-export "$(sops -d secret.env | xargs)"
+export $(sops -d secret.env | xargs)
 
 kubectl -n=moov delete secret ${SECRET_NAME} 2> /dev/null
 kubectl -n=moov \
 create secret generic ${SECRET_NAME} \
---from-literal="MARIADB_ROOT_USER=$MARIADB_ROOT_USER" \
---from-literal="MARIADB_ROOT_PASSWORD=$MARIADB_ROOT_PASSWORD" \
---from-literal="SPRING_DATASOURCE_USERNAME=$MARIADB_ROOT_USER" \
---from-literal="SPRING_DATASOURCE_PASSWORD=$MARIADB_ROOT_PASSWORD"
-
-echo "added  MARIADB_ROOT_USER=$MARIADB_ROOT_USER"
-echo "added  MARIADB_ROOT_PASSWORD=$MARIADB_ROOT_PASSWORD"
-echo "added  SPRING_DATASOURCE_USERNAME=$MARIADB_ROOT_USER"
-echo "added  SPRING_DATASOURCE_PASSWORD=$MARIADB_ROOT_PASSWORD"
+--from-literal="DB_HOST=$DB_HOST" \
+--from-literal="DB_USER=$DB_USER" \
+--from-literal="DB_PASSWORD=$DB_PASSWORD" \
+--from-literal="DB_NAME=$DB_NAME" \
+--from-literal="DB_PORT=$DB_PORT" \
+--from-literal="POSTGRES_PASSWORD=$DB_PASSWORD" \
+--from-literal="POSTGRES_USER=$DB_USER" \
+--from-literal="POSTGRES_DB=$DB_NAME" \
+--from-literal="JWT_SECRET=$JWT_SECRET" \
+--from-literal="JWT_REFRESH_SECRET=$JWT_REFRESH_SECRET" \
+--from-literal="EMAIL=$EMAIL" \
+--from-literal="EMAIL_PASSWORD=$EMAIL_PASSWORD" \
+--from-literal="VITE_MAPS_API_KEY=$VITE_MAPS_API_KEY"
