5e6653eaca5179d0695f23277e3ddbecb470143d
Bastian SOMON <bastian.somon@gmail.com>
2023-10-27 14:05:33 +0000

    Fix: CI

--- infra/create_registry_secret.sh
+++ infra/create_registry_secret.sh
@@ -1,3 +1,5 @@
+kubectl delete secret registry-creds -n moov
+
 kubectl create secret generic registry-creds \
     --from-file=.dockerconfigjson=/root/.docker/config.json \
     --type=kubernetes.io/dockerconfigjson \
